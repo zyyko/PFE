9f3ec70031142eaea42f480511d059499c665436
zyko <zyko.taouaf@gmail.com>
2024-03-02 21:49:07 +0100

    done

--- resources/views/partials_users/footer.blade.php
+++ resources/views/partials_users/footer.blade.php
@@ -98,6 +98,7 @@ ul{
 
 </style>
 <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+</head>
 <body>
 
   <footer class="footer">
